2b986b28dd9029b6320ea09c78cd84849cf45461
Djadmenov <82372276+Djadmenov@users.noreply.github.com>
2022-05-13 16:00:07 +0600

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,18 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  arr1=[1,2,3];
+  arr2=[1,2,4];
+  console.log(JSON.stringify(arr1)==JSON.stringify(arr2));
 
   return result; // boolean
 }
+compareArrays();//без этого ни как не могу
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  
 
   return resultArr; // array
 }
